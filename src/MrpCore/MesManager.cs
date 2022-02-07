63ee5eb9ffb88a56760a20d84bb300af5e9f1cd5
Matt Jarvis <mattj23@gmail.com>
2022-02-07 13:48:38 -0500

    manager started

---
+++ src/MrpCore/MesManager.cs
@@ -0,0 +1,27 @@
+﻿using MrpCore.Models;
+
+namespace MrpCore;
+
+public class MesManager<TProductType, TUnitState, TProductUnit, TRouteOperation, TUnitOperation, TOperationResult> 
+                            where TUnitState : UnitStateBase
+                            where TProductType : ProductTypeBase
+                            where TProductUnit : ProductUnitBase<TProductType>
+                            where TRouteOperation : RouteOperationBase<TProductType>
+                            where TUnitOperation : UnitOperationBase<TProductType, TUnitState, TProductUnit, TRouteOperation>
+                            where TOperationResult : OperationResultBase<TProductType, TUnitState, TProductUnit, TRouteOperation, TUnitOperation>
+{
+    private readonly MesContext<TProductType, TUnitState, TProductUnit, TRouteOperation, TUnitOperation,
+        TOperationResult> _db;
+
+    protected MesManager(MesContext<TProductType, TUnitState, TProductUnit, TRouteOperation, TUnitOperation, TOperationResult> db)
+    {
+        _db = db;
+    }
+
+    public IQueryable<TProductType> ProductTypes()
+    {
+        return _db.Types;
+    }
+
+
+}
